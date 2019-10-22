6daa44c21b52fd22b7a5180cd0c4eaf562ae2023
acmerInTw <linuxkernelisgood@gmail.com>
2019-10-22 23:21:24 +0800

    [枚舉][0001] 完成
    1. [枚舉][0001] 完成
    2. 修改題目清單

---
+++ 演算法/枚舉/0001.cpp
@@ -0,0 +1,67 @@
+/*    Largest Rectangle in a Histogram
+ *    URL : http://poj.org/problem?id=2559
+ */
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+#define NUMS 10
+typedef long long LL;
+
+int main()
+{
+    int n, i;
+    int Top;
+    LL MaxArea, TmpArea;
+    int RecHeight[NUMS];  /* 紀錄輸入的長條圖高度 */
+    int StackIndex[NUMS]; /* 向右或向左尋找邊界時,一邊記錄經過的長條圖index*/
+    int LeftIndex[NUMS];  /* 紀錄在左邊處小於該長條圖高度的邊界index */
+    int RightIndex[NUMS]; /* 紀錄在右邊處小於該長條圖高度的邊界index */
+
+    while (scanf("%d", &n)) {
+        if (n == 0)
+            break;
+
+        for (i = 0; i < n; ++i)
+            scanf("%d", &RecHeight[i]);
+
+        MaxArea = 0;
+        Top = -1;
+        memset(LeftIndex, -1, sizeof(LeftIndex));
+        memset(RightIndex, -1, sizeof(RightIndex));
+
+        /* 向右尋找邊界 */
+        for (i = 0; i<n; ++i) {
+            while (Top > -1 && RecHeight[StackIndex[Top]] > RecHeight[i]) {
+                RightIndex[StackIndex[Top]] = i;
+                --Top;
+            }
+            StackIndex[++Top] = i;
+        }
+
+        Top = -1;
+
+        /* 向左尋找邊界 */
+        for (i = n-1; i>=0; --i) {
+            while (Top > -1 && RecHeight[StackIndex[Top]] > RecHeight[i]) {
+                LeftIndex[StackIndex[Top]] = i;
+                --Top;
+            }
+            StackIndex[++Top] = i;
+        }
+
+        /* 每個長條圖左右兩邊邊界找到後就可以算出以該長條圖為中心向旁邊擴展的最大長方形 */
+        for (i = 0; i<n; ++i) {
+            int r = (RightIndex[i] == -1) ? n : RightIndex[i];
+            int l = (LeftIndex[i] == -1) ? -1 : LeftIndex[i];
+            TmpArea = (r - l - 1) * ( (LL)RecHeight[i]);
+            if (TmpArea > MaxArea)
+                MaxArea = TmpArea;
+        }
+
+        cout << MaxArea << endl;
+
+    }
+    return 0;
+}
